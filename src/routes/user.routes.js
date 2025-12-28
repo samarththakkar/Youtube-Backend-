e3fe0aa3ca61e307dda7cf4e0ed7bb3fea3b9365
Samarth <samarththakkar841@gmail.com>
2025-12-28 12:45:46 +0530

    fix: correct typo in watch history route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -42,5 +42,5 @@ router.route("/update-account").patch(verifyJWT,updateAccountDetails);
 router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar);
 router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage);
 router.route("/channel/:username").get(verifyJWT,getUserChannelProfile);
-router.route("/watch-history").gwt(verifyJWT,getWatchHistory);
+router.route("/watch-history").get(verifyJWT,getWatchHistory);
 export default router;
